07eaf0b4328e1d16744d74daff65f5c440b25875
M@ <mattdunlap+github@gmail.com>
2014-01-09 20:54:53 -0500

    initial commit, got docopt going

---
+++ konane.py
@@ -0,0 +1,23 @@
+#!/usr/bin/evn python
+""" Tufts Comp 131 Konane Skeleton
+
+Play a game of Konane between two players, either of whom can be 1) Human 
+2) Random or 3) An AI of your construction!
+
+Usage:
+  konane
+
+Options:
+  -h --help         Show this screen.
+"""
+
+from docopt import docopt
+
+
+
+
+
+
+
+if __name__ == "__main__":
+  arguments = docopt(__doc__, version="Konane v1.0")
